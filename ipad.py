304b7b5c4da5d1b7ee25059fadd722048a9a5674
Andrei Olariu <andreidaniel.olariu@g.unibuc.ro>
2012-02-05 13:04:19 +0200

    Added a try except for pipeline functions

--- ipad.py
+++ ipad.py
@@ -9,7 +9,8 @@ logging.basicConfig(
 logger = logging.getLogger('ipad.py')
 logger.debug('Starting the monitor')
 
-p = Pipeline(['ipad3', '"ipad 3"', '"new ipad"', 'ipad2s', '"ipad 2s"', 'ipad2', '"ipad 2"', 'ipadmini', '"ipad mini"'], savefile='ipaddata', savedelta=3600)
+p = Pipeline(['ipad3', '"ipad 3"', '"new ipad"', 'ipad2s', '"ipad 2s"', 'ipad2', 
+    '"ipad 2"', 'ipadmini', '"ipad mini"'], savefile='ipaddata', savedelta=3600)
 
 def strip_useless_info(input_queue=None, output_queue=None, keep_monitoring=None):
     #... because less is more
--- lib/pipeline.py
+++ lib/pipeline.py
@@ -7,6 +7,11 @@ from lib.api.twitter import fetch_tweets
 logger = logging.getLogger('pipeline')
 
 class Pipeline:
+    '''
+        Defines a pipeline for polling an API and processing and saving the 
+        results
+        At this time it only works on twitter. Will expand (perhaps) in time
+    '''
     
     def __init__(self, keywords, interval=10, per_interval=100, \
             savefile=None, savedelta=3600):
@@ -18,9 +23,14 @@ class Pipeline:
         self.savedelta = savedelta
         
     def add_function(self, function, params):
+        # Add a 'processing' function to the pipeline
+        # The function must accept the parameters: input_queue, output_queue
+        # and keep_monitoring
+        # Others parameters can be added in the params dictionary
         self.functions.append((function, params))
         
     def get_interval(self, l):
+        # Adapt the polling interval for the fetching function
         if self.per_interval == 0:
             return self.interval
         if l < 0.3 * self.per_interval:
@@ -35,30 +45,50 @@ class Pipeline:
         return self.interval
     
     def keep_monitoring(self):
+        # See stop() for info
         return self.running
         
     def stop(self):
+        # Stops the pipeline
+        # For use when in ipython
         self.running = False
     
     def run(self):
+        # Start the pipeline threads
         self.running = True
+        
+        # Execute the 'fetcher' functions
         old_queue = Queue.Queue()
         self.functions[0][1]['queue'] = old_queue
-        threading.Thread(target=self.functions[0][0], \
-                kwargs=self.functions[0][1]).start()
+        threading.Thread(
+                target=self.capsule, 
+                args=(self.functions[0][0], self.functions[0][1])
+        ).start()
+        
+        # Execute the 'processing' functions
         for function in self.functions[1:]:
             new_queue = Queue.Queue()
             function[1]['input_queue'] = old_queue
             function[1]['output_queue'] = new_queue
             function[1]['keep_monitoring'] = self.keep_monitoring
-            threading.Thread(target=function[0], kwargs=function[1]).start()
+            threading.Thread(
+                    target=self.capsule, 
+                    args=(function[0], function[1])
+            ).start()
             old_queue = new_queue
+            
+        # Execute the 'writer' function, if requested
+        # If not, return the output queue
         if self.savefile:
-            threading.Thread(target=self.writer, kwargs={'queue': old_queue}).start()
+            threading.Thread(
+                    target=self.capsule, 
+                    args=(self.writer, {'queue': old_queue})
+            ).start()
         else:
             self.output = old_queue
         
     def monitor_twitter(self, queue=None, keywords=None):
+        # Fetcher function, for monitoring twitter
         next_run = time.time()
         id_cache = set([])
         while self.keep_monitoring():
@@ -73,6 +103,7 @@ class Pipeline:
             time.sleep(1)
             
     def writer(self, queue=None):
+        # Write processed data to file
         next_run = time.time() + self.savedelta
         while self.keep_monitoring():
             if next_run <= time.time():
@@ -87,3 +118,13 @@ class Pipeline:
                 next_run += self.savedelta
             time.sleep(1)
             
+    def capsule(self, function, kwargs):
+        # Encapsulates functions, to catch errors and restart them
+        while True:
+            try:
+                function(**kwargs)
+                break
+            except Exception, e:
+                logger.debug('Error in %s: %s - using %s' % (function, e, kwargs))
+                time.sleep(4)
+                
